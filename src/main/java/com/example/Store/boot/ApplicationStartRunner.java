b1f462cdea75713d7af866263b8f724f19f8bbb5
bakai <bakaiberdibekov@gmail.com>
2021-12-25 10:30:06 +0200

    add heroku

--- src/main/java/com/example/Store/boot/ApplicationStartRunner.java
+++ src/main/java/com/example/Store/boot/ApplicationStartRunner.java
@@ -20,21 +20,21 @@ public class ApplicationStartRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        UserRole roleAdmin = new UserRole();
-        roleAdmin.setRoleName("ROLE_ADMIN");
-        userRoleService.createUserRole(roleAdmin);
-
-        UserRole roleClient = new UserRole();
-        roleClient.setRoleName("ROLE_CLIENT");
-        userRoleService.createUserRole(roleClient);
-
-        UserModel admin = new UserModel();
-        admin.setFullName("Bakai Berdibekov");
-        admin.setLogin("admin");
-        admin.setPassword("admin");
-        admin.setIsActive(1L);
-        admin.setUserRoleId(roleAdmin.getId());
-        userService.createUser(admin);
+//        UserRole roleAdmin = new UserRole();
+//        roleAdmin.setRoleName("ROLE_ADMIN");
+//        userRoleService.createUserRole(roleAdmin);
+//
+//        UserRole roleClient = new UserRole();
+//        roleClient.setRoleName("ROLE_CLIENT");
+//        userRoleService.createUserRole(roleClient);
+//
+//        UserModel admin = new UserModel();
+//        admin.setFullName("Bakai Berdibekov");
+//        admin.setLogin("admin");
+//        admin.setPassword("admin");
+//        admin.setIsActive(1L);
+//        admin.setUserRoleId(roleAdmin.getId());
+//        userService.createUser(admin);
 
     }
 }
